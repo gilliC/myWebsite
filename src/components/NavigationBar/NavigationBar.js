d33b2232de5bd48c98ab2d54c862a9c67bcb1398
Gilli Carmon <gillicarmon@gmail.com>
2018-08-31 17:12:35 +0300

    #19 fixed

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,36 +1,64 @@
-import React, {Component} from 'react'; import PropTypes from 'prop-types';
-import {Navbar, NavItem, Nav} from 'react-bootstrap'; import {withRouter} from
-'react-router-dom';
+import React, {Component} from 'react';
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
+import {withRouter} from 'react-router-dom';
 
-import logo from '../../drawable/websiteLogo.png'; import NavigationNav from
-'./NavigationNav'; import './NavigationBar.scss';
+import logo from '../../drawable/websiteLogo.png';
+import NavigationNav from
+        './NavigationNav';
+import './NavigationBar.scss';
 
-class NavigationBar extends Component { static defaultProps = { pages: [ {path:
-'projects', title: 'Projects'}, {path: 'articles', title: 'Articles'}, {path:
-'dashboard', title: 'Dashboard'}, {path: 'ecologicaltips', title: 'Ecological
-	Tips'}, ], };
+class NavigationBar extends Component {
+    static defaultProps = {
+        pages: [
+            {path: 'projects', title: 'Projects'},
+            {path: 'articles', title: 'Articles'},
+            {path: 'dashboard', title: 'Dashboard'},
+            {path: 'ecologicaltips', title: 'EcologicalTips'},
+        ],
+    };
 
-  constructor(props) { super(props); this.state = {isHovered: false};
-	  this.onMouseEnter = this.onMouseEnter.bind(this); this.onMouseLeave =
-		  this.onMouseLeave.bind(this); }
+    constructor(props) {
+        super(props);
+        this.state = {isHovered: false};
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave =
+            this.onMouseLeave.bind(this);
+    }
 
-  onMouseEnter() { this.setState({isHovered: true}); }
+    onMouseEnter() {
+        this.setState({isHovered: true});
+    }
 
-  onMouseLeave() { this.setState({isHovered: false}); }
+    onMouseLeave() {
+        this.setState({isHovered: false});
+    }
 
-  render() { let logoClassName = this.state.isHovered ? 'app-logo-hover' :
-		  'app-logo'; const pathname = this.props.location.pathname;
+    render() {
+        let logoClassName = this.state.isHovered ? 'app-logo-hover' :
+            'app-logo';
+        const pathname = this.props.location.pathname;
 
-    return ( <div> <Navbar fluid={true} componentClass="app-nav-bar">
-		    <Navbar.Header> <Navbar.Brand> <Nav bsClass="app-brand"
-					    pullLeft> <NavItem href="/">GC
-						    Website</NavItem> </Nav>
-	    </Navbar.Brand> </Navbar.Header> {this.props.pages.map(page => {
-		    return ( <NavigationNav page={page} key={page.path}
-			    localPath={pathname} />); })} <Nav
-	    bsClass="logo-div" pullRight> <NavItem> <img src={logo}
-	    className={logoClassName} alt="logo"
-	    onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}
-	    /> </NavItem> </Nav> </Navbar> </div>); } }
+        return (<div><Navbar fluid={true} componentClass="app-nav-bar">
+                <Navbar.Header> <Navbar.Brand> <Nav bsClass="app-brand"
+                                                    pullLeft> <NavItem href="/">GC
+                    Website</NavItem> </Nav>
+                </Navbar.Brand> </Navbar.Header> {this.props.pages.map(page => {
+                return (<NavigationNav page={page} key={page.path}
+                                       localPath={pathname}/>);
+            })}
+                <Nav
+                    bsClass="logo-div" pullRight>
+                    <NavItem>
+                        <img src={logo}
+                             className={logoClassName} alt="logo"
+                             onMouseEnter={this.onMouseEnter}
+                             onMouseLeave={this.onMouseLeave}/>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+            </div>
+        );
+    }
+}
 
- export default withRouter(NavigationBar);
+export default withRouter(NavigationBar);
