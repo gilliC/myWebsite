2fae3370965b1f2d1c24743e8733110b6a817a23
GilliCarmon <gillic@rumble.co.il>
2020-04-09 19:05:13 +0300

    update react for hooks

--- src/screens/EcologicalTips/components/EcoTipItem/index.js
+++ src/screens/EcologicalTips/components/EcoTipItem/index.js
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TipContent from "./components/TipContent";
 import EcoItemTitle from "./components/EcoItemTitle";
 import EcoItemContainer from "./components/EcoItemContainer";
 
-export default class EcoTipItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { clicked: false };
-  }
+const EcoTipItem = (props) => {
+  const [clicked, setClicked] = useState(false);
 
-  onClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+  const onClick = () => {
+    setClicked(!clicked);
   };
-  render() {
-    const { img, info, src, title } = this.props.item;
-    const { clicked } = this.state;
-    return (
-      <EcoItemContainer img={img} src={src} clicked={clicked} onClick={this.onClick}>
-        <EcoItemTitle clicked={clicked}>{title}</EcoItemTitle>
-        <TipContent info={info} clicked={clicked} />
-      </EcoItemContainer>
-    );
-  }
-}
+  const { img, info, src, title } = props.item;
+  return (
+    <EcoItemContainer img={img} src={src} clicked={clicked} onClick={onClick}>
+      <EcoItemTitle clicked={clicked}>{title}</EcoItemTitle>
+      <TipContent info={info} clicked={clicked} />
+    </EcoItemContainer>
+  );
+};
 
 EcoTipItem.propTypes = {
   item: PropTypes.shape({
@@ -34,3 +28,4 @@ EcoTipItem.propTypes = {
     title: PropTypes.string,
   }),
 };
+export default EcoTipItem;
